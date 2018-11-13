5acc3e7c73987a3515afe2ec2463c36cc1aca2d6
Rheimus <iessa.pragg@gmail.com>
2018-11-13 19:47:33 +1300

    Configurable host & public dir

--- update.bat
+++ update.bat
@@ -1,18 +1,29 @@
 @echo off
+
+set host=https://puddle.zone
+set public_files_dir=minecraft
+
 echo Checking for latest version of ServerSync software
-for %%f in (serversync*.jar) do (SET serversync=%%~nxf)
-call winhttpjs.bat https://puddle.zone/minecraft/sslatest.txt -saveTo sslatest.txt
-set /p Build=<sslatest.txt
-echo Current version %serversync%
-echo Server version %Build%
-if "%serversync%" == "%Build%" (
-	echo Version current.  Launching ServerSync.
+
+rem Get client serversync version
+for %%f in (serversync*.jar) do (SET client_build=%%~nxf)
+
+rem Fetch serversync version from host
+call winhttpjs.bat %host%/%public_files_dir%/sslatest.txt -saveTo sslatest.txt
+set /p server_build=<sslatest.txt
+
+echo Current version %client_build%
+echo Server version %server_build%
+
+if "%client_build%" == "%server_build%" (
+	echo Version current.
 )
-if not "%serversync%" == "%Build%" (
+if not "%client_build%" == "%server_build%" (
 	echo ServerSync version mismatch.  Downloading current version from the server.
-	call winhttpjs.bat https://puddle.zone/minecraft/%Build% -saveTo %Build%
+	call winhttpjs.bat %host%/%public_files_dir%/%server_build% -saveTo %server_build%
 	echo Download complete.  Removing old version.
-	del %serversync%
-	echo Launching ServerSync.
+	del %client_build%
 )
-java -jar %Build% progress-only
+
+echo Launching ServerSync.
+java -jar %server_build% progress-only
